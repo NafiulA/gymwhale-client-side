539f3bd96d57195d52d733260b276e28ef1e84cc
Nafiul Alam <nafiulalam30@gmail.com>
2022-04-17 19:42:48 +0600

    banner done

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import bannerimg from "../../assets/images/bannerImage.jpg";
+import "./banner.css";
 const Banner = () => {
     return (
         <div>
-            <div className='min-h-full lg:min-h-screen w-100 ' style={{ backgroundImage: `url(${bannerimg})`, backgroundSize: "cover" }}>
+            <div className='mainDiv min-h-fit lg:min-h-screen w-100 block md:grid md:grid-cols-1 lg:grid-cols-2 justify-center items-center' style={{ backgroundImage: `url(${bannerimg})`, backgroundSize: "cover", backgroundPosition: "center", backgroundPositionX: "right" }}>
+                <div className='order-2 lg:order-1 md:pb-16 lg:pb-0'>
+                    <h1 className='text-5xl md:text-7xl p-6 md:p-11 font-bold text-slate-100 backdrop-contrast-50'>Proper Fitness is key to<span className='text-blue-900'>Healthy living</span></h1>
+                    <p className='text-2xl pb-8 px-6 md:px-11 text-white backdrop-contrast-50'>​That's why my mission is to help people enjoy life to its fullest by supervising and creating expertly designed fitness programs. I value an individuals need to improve their quality of life​.</p>
+                </div>
+                <div className='hidden order-1 lg:order-2 md:block'>
+                    <div className='min-h-[400px] w-full'></div>
+                </div>
             </div>
 
         </div >
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,7 @@ import { MenuIcon, XIcon } from '@heroicons/react/solid';
 const Header = () => {
     const [open, setOpen] = useState(false);
     return (
-        <div>
+        <div className='sticky top-0 z-50'>
             <nav className="border-gray-200 px-2 py-2.5 bg-gray-800">
                 <div className="container flex flex-wrap justify-between items-center mx-auto">
                     <Link to="/" className="flex items-center">
@@ -15,7 +15,7 @@ const Header = () => {
                     <div onClick={() => setOpen(!open)} className='w-8 ml-auto py-2 text-white md:hidden'>
                         {open ? <XIcon></XIcon> : <MenuIcon></MenuIcon>}
                     </div>
-                    <div className={`absolute ${open ? 'top-14 right-0 left-0' : 'top-[-300px] right-0 left-0'} duration-200 ease-linear bg-gray-800 overflow-hidden w-full md:w-auto md:static`} id="mobile-menu">
+                    <div className={`absolute ${open ? 'top-14 right-0 left-0' : 'top-[-300px] right-0 left-0'} duration-200 ease-linear bg-gray-800 overflow-hidden w-full z-50 md:w-auto md:static`} id="mobile-menu">
                         <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
                             <li>
                                 <CustomLink to="/">Home</CustomLink>
